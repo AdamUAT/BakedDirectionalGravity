e73081149fbe49e59da5b8f3db55a240a8c15177
AdamUAT <115857693+AdamUAT@users.noreply.github.com>
2023-11-26 18:42:27 -0700

    Added buttons for a box and cube, and added sphere logic

--- Assets/AdamUAT/Scripts/Gravity/GravityObject.cs
+++ Assets/AdamUAT/Scripts/Gravity/GravityObject.cs
@@ -47,7 +47,7 @@ public class GravityObject : MonoBehaviour
     }
 
     //Will automatically populate a preset of gravity points for regular objects like a cube or sphere.
-    public void ApplyPreset(GravityManager.RegularShapes shape, GravityManager.CoveragePresets coverage, float density, List<float> dimensions)
+    public void ApplyPreset(GravityManager.RegularShapes shape, GravityManager.CoveragePresets coverage, float density, bool shouldOverride, List<float> dimensions)
     {
         //Find how many variables are needed for the preset.
         int dimensionsCount = 0;
@@ -75,6 +75,19 @@ public class GravityObject : MonoBehaviour
             return;
         }
 
+        //Density has a hard minimum of 0.05 so there won't be too many gravity points added.
+        if(density <= 0.05)
+        {
+            Debug.LogError("Density must be greater than 0.05 for a preset to be applied correctly.");
+            return;
+        }
+
+        //If the preset should override the points instead of add to them.
+        if(shouldOverride)
+        {
+            gravityPoints.Clear();
+        }
+
         //Call the function for the shape.
         switch (shape)
         {
@@ -96,7 +109,29 @@ public class GravityObject : MonoBehaviour
 
     private void PresetSphere(GravityManager.CoveragePresets coverage, float density, float radius)
     {
-        Debug.Log("Sphere");
+        int n = (int)(4 * Mathf.PI * radius * radius / density / density);
+
+        float inc = Mathf.PI * (3 - Mathf.Sqrt(5));
+        float off = 2.0f / n;
+        float x = 0;
+        float y = 0;
+        float z = 0;
+        float r = 0;
+        float phi = 0;
+
+        for (var k = 0; k < n; k++)
+        {
+            y = k * off - 1 + (off / 2);
+            r = Mathf.Sqrt(1 - y * y);
+            phi = k * inc;
+            x = Mathf.Cos(phi) * r;
+            z = Mathf.Sin(phi) * r;
+
+            GravityData gravityPoint = new GravityData();
+            gravityPoint.Position = (new Vector3(x, y, z)) * radius + gameObject.transform.position;
+            gravityPoint.Direction = gravityPoint.Position - gameObject.transform.position;
+            gravityPoints.Add(gravityPoint);
+        }
     }
 
     private void PresetCube(GravityManager.CoveragePresets coverage, float density, float edgeLength)
@@ -158,6 +193,7 @@ public class GravityObject_Inspector : Editor
         listView.selectionChanged += objects => Debug.Log($"Selected: {string.Join(", ", objects)}");
         listView.itemsChosen += objects => Debug.Log($"Double-clicked: {string.Join(", ", objects)}");
         listView.style.flexGrow = 1.0f;
+        listView.style.maxHeight = 300;
 
         myInspector.Add(listView);
         #endregion ListView
@@ -168,7 +204,15 @@ public class GravityObject_Inspector : Editor
         //Bind the buttons to the functions
         presetDisplay.onCreateSpherePreset.AddListener((float radius) =>
         {
-            gravityObject.ApplyPreset(GravityManager.RegularShapes.Sphere, presetDisplay.GetCoverage(), presetDisplay.GetDensity(), new List<float>() { radius });
+            gravityObject.ApplyPreset(GravityManager.RegularShapes.Sphere, presetDisplay.GetCoverage(), presetDisplay.GetDensity(), presetDisplay.IsOverride(), new List<float>() { radius });
+        });
+        presetDisplay.onCreateBoxPreset.AddListener((Vector3 dimensions) =>
+        {
+            gravityObject.ApplyPreset(GravityManager.RegularShapes.Box, presetDisplay.GetCoverage(), presetDisplay.GetDensity(), presetDisplay.IsOverride(), new List<float>() { dimensions.x, dimensions.y, dimensions.z });
+        });
+        presetDisplay.onCreateCubePreset.AddListener((float edgeLength) =>
+        {
+            gravityObject.ApplyPreset(GravityManager.RegularShapes.Cube, presetDisplay.GetCoverage(), presetDisplay.GetDensity(), presetDisplay.IsOverride(), new List<float>() { edgeLength });
         });
 
 
@@ -228,7 +272,10 @@ public class GravityObject_Inspector : Editor
     {
         private EnumField coverage;
         private FloatField density;
+        private Toggle shouldOverride;
         public UnityEvent<float> onCreateSpherePreset = new UnityEvent<float>();
+        public UnityEvent<Vector3> onCreateBoxPreset = new UnityEvent<Vector3>();
+        public UnityEvent<float> onCreateCubePreset = new UnityEvent<float>();
 
         public PresetDisplay()
         {
@@ -249,6 +296,7 @@ public class GravityObject_Inspector : Editor
             VisualElement coverageSettings = new VisualElement();
             settings.contentContainer.Add(coverageSettings);
             coverageSettings.style.flexDirection = FlexDirection.Row;
+            coverageSettings.style.marginRight = 20;
 
             Label coverageLabel = new Label();
             coverageLabel.text = "Coverage: ";
@@ -261,7 +309,6 @@ public class GravityObject_Inspector : Editor
             coverage.style.height = 20;
             coverage.style.minWidth = 100;
             coverage.style.maxWidth = 150;
-            coverage.style.marginRight = 20;
 
             #endregion Coverage
             
@@ -269,6 +316,7 @@ public class GravityObject_Inspector : Editor
             VisualElement densitySettings = new VisualElement();
             settings.contentContainer.Add(densitySettings);
             densitySettings.style.flexDirection = FlexDirection.Row;
+            densitySettings.style.marginRight = 20;
 
             Label densityLabel = new Label();
             densityLabel.text = "Density: ";
@@ -280,13 +328,31 @@ public class GravityObject_Inspector : Editor
             density.style.height = 20;
             density.style.minWidth = 50;
             density.style.maxWidth = 100;
+            density.value = 1;
 
             #endregion Density
+
+            #region Override
+            VisualElement overrideSettings = new VisualElement();
+            settings.contentContainer.Add(overrideSettings);
+            overrideSettings.style.flexDirection = FlexDirection.Row;
+
+            Label overrideLabel = new Label();
+            overrideLabel.text = "Should override current points: ";
+            overrideSettings.contentContainer.Add(overrideLabel);
+            overrideLabel.style.unityTextAlign = TextAnchor.MiddleLeft;
+
+            shouldOverride = new Toggle();
+            overrideSettings.contentContainer.Add(shouldOverride);
+            shouldOverride.style.height = 20;
+            shouldOverride.style.paddingTop = 5;
+
+            #endregion Override
 
             #endregion Settings
 
             #region Buttons
-            
+
             VisualElement presets = new VisualElement();
             dropdown.contentContainer.Add(presets);
 
@@ -320,12 +386,82 @@ public class GravityObject_Inspector : Editor
 
             #endregion Sphere
 
+            #region Box
+            Foldout boxDropdown = new Foldout();
+            boxDropdown.text = "Box";
+            boxDropdown.value = false;
+            boxDropdown.style.paddingBottom = 5;
+            boxDropdown.style.paddingRight = 10;
+            presets.contentContainer.Add(boxDropdown);
+
+            VisualElement boxParameters = new VisualElement();
+            boxDropdown.contentContainer.Add(boxParameters);
+            boxParameters.style.flexDirection = FlexDirection.Row;
+
+            Label boxDimensionsLabel = new Label();
+            boxDimensionsLabel.text = "Dimensions: ";
+            boxParameters.contentContainer.Add(boxDimensionsLabel);
+            boxDimensionsLabel.style.unityTextAlign = TextAnchor.MiddleLeft;
+
+            Vector3Field boxDimensions = new Vector3Field();
+            boxParameters.contentContainer.Add(boxDimensions);
+            boxDimensions.style.height = 20;
+            boxDimensions.style.minWidth = 200;
+            boxDimensions.style.maxWidth = 300;
+
+            Action onBoxButtonClicked = () => { CreateBoxPreset(boxDimensions.value); };
+            Button boxButton = new Button(onBoxButtonClicked);
+            boxDropdown.contentContainer.Add(boxButton);
+            boxButton.text = "Apply Box Preset";
+
+            #endregion Box
+
+            #region Cube
+            Foldout cubeDropdown = new Foldout();
+            cubeDropdown.text = "Cube";
+            cubeDropdown.value = false;
+            cubeDropdown.style.paddingBottom = 5;
+            cubeDropdown.style.paddingRight = 10;
+            presets.contentContainer.Add(cubeDropdown);
+
+            VisualElement cubeParameters = new VisualElement();
+            cubeDropdown.contentContainer.Add(cubeParameters);
+            cubeParameters.style.flexDirection = FlexDirection.Row;
+
+            Label cubeDimensionsLabel = new Label();
+            cubeDimensionsLabel.text = "Edge Length: ";
+            cubeParameters.contentContainer.Add(cubeDimensionsLabel);
+            cubeDimensionsLabel.style.unityTextAlign = TextAnchor.MiddleLeft;
+
+            FloatField cubeEdge = new FloatField();
+            cubeParameters.contentContainer.Add(cubeEdge);
+            cubeEdge.style.height = 20;
+            cubeEdge.style.minWidth = 50;
+            cubeEdge.style.maxWidth = 100;
+
+            Action oncubeButtonClicked = () => { CreateCubePreset(cubeEdge.value); };
+            Button cubeButton = new Button(oncubeButtonClicked);
+            cubeDropdown.contentContainer.Add(cubeButton);
+            cubeButton.text = "Apply Cube Preset";
+
+            #endregion Cube
+
             #endregion Buttons
         }
 
         private void CreateSpherePreset(float radius)
         {
             onCreateSpherePreset?.Invoke(radius);
+        }
+
+        private void CreateBoxPreset(Vector3 dimensions)
+        {
+            onCreateBoxPreset?.Invoke(dimensions);
+        }
+
+        private void CreateCubePreset(float edgeLength)
+        {
+            onCreateCubePreset?.Invoke(edgeLength);
         }
 
         public float GetDensity()
@@ -336,6 +472,11 @@ public class GravityObject_Inspector : Editor
         public GravityManager.CoveragePresets GetCoverage()
         {
             return (GravityManager.CoveragePresets)coverage.value;
+        }
+
+        public bool IsOverride()
+        {
+            return shouldOverride.value;
         }
     }
 }
